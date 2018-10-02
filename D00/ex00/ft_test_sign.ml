2b3ffaf916472a0ffc2bbf6610b9a4d95cd0a8ff
Nabil GALOUL <nagaloul@e1r9p6.42.fr>
2018-10-02 14:30:38 +0200

    finish

--- D00/ex00/ft_test_sign.ml
+++ D00/ex00/ft_test_sign.ml
@@ -6,8 +6,13 @@
 (*   By: nagaloul <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2018/09/27 14:17:08 by nagaloul          #+#    #+#             *)
-(*   Updated: 2018/09/27 14:18:01 by nagaloul         ###   ########.fr       *)
+(*   Updated: 2018/10/02 14:30:14 by nagaloul         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-
+let ft_test_sign n = 
+    if (n < 0) then
+      print_endline "negative"
+    else
+      print_endline "positive" ;;
+ft_test_sign (-5);;
